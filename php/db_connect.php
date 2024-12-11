0ecb123719210b00ef18d9e88813ab601b98d0c1
Rosmarin <forwot.6.8lvl@gmail.com>
2024-12-11 19:27:54 +0400

    Тестирование взаимодействия с БД +

--- php/db_connect.php
+++ php/db_connect.php
@@ -6,7 +6,8 @@ $db_user = 'root';
 $db_name = 'gpteam';
 
 $conn = new mysqli($db_host, $db_user, '', $db_name);
-    
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
--- php/test.php
+++ php/test.php
@@ -1,22 +1,44 @@
+<?php
+    session_start();
+    include './db_connect.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Hello World!!</h1>
-    <button id="button">insert value</button>
-    <table id="users_table">
-        <!-- The table will be populated by the getUsers() function -->
-    </table>
+    <form method="POST">
+        <button type="submit" name="generate" class="buttonClass">Run SQL</button>
+        <button type="submit" name="select" class="buttonClass">Show Data</button>
+        <button type="submit" name="truncate" class="buttonClass">Truncate Table</button>
+    </form>
     <?php
-        session_start();
-        include './db_connect.php';
-        
-
+        if(isset($_POST['generate'])) {
+            $value = rand(5, 15);
+            $value1 = rand(5, 15);
+            $sql = "INSERT INTO test (login, pass) VALUES ($value, $value1)";
+            mysqli_query($conn, $sql);
+        }
+        if(isset($_POST['select'])) {
+            $sql = "SELECT * FROM test";
+            $result = mysqli_query($conn, $sql);
+            
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "Login: " . $row['login'] . " - Password: " . $row['pass'] . "<br>";
+            }
+        }
+        if(isset($_POST['truncate'])) {
+            $sql = "TRUNCATE TABLE test";
+            mysqli_query($conn, $sql);
+        }
     ?>
+
+
     <script src="../js/script.js">
 </body>
 </html>
