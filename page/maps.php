321183480be32e9901d14157671aecffbf547c46
Rosmarin <thegreattimofey.devyatkin@gmail.com>
2025-05-28 17:11:47 +0400

    map scroll update

--- page/maps.php
+++ page/maps.php
@@ -1,4 +1,4 @@
 <!--карта-->
 <div class="map" id="map">
-<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7a5dc283fd69b62170b5d095c3767093f5e812657dd9f1965b0fed38a7ad3f67&amp;width=100%25&amp;height=240&amp;lang=ru_RU&amp;scroll=true"></script>
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7a5dc283fd69b62170b5d095c3767093f5e812657dd9f1965b0fed38a7ad3f67&amp;width=100%25&amp;height=240&amp;lang=ru_RU&amp;scroll=false"></script>
 </div>
